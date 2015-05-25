b057ec13ed0282304a8648718657144102424459
Semigradsky <d.semigradsky@itransition.com>
2015-05-25 18:04:40 +0300

    Added react-a11y

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,8 @@ module.exports = {
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({ DEV: 'true' })
   ]
 
 };
